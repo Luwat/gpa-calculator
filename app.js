5b0793678a25545a6f7dc72573f4ff39f8287ad9
luwat <oluwatobioreniyi@gmail.com>
2022-08-16 09:13:46 +0100

    lil adjustment

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const form = document.querySelector('.form')
 const addCourseBtn = document.querySelector('.submit')
-const formGroup = document.createElement('div')
-const labelCourse = document.createElement('label')
-const inputCourse = document.createElement('input')
-const labelGP = document.createElement('label')
-const select = document.createElement('select')
-const optionA = document.createElement('option')
-const optionB = document.createElement('option')
-const optionC = document.createElement('option')
-const optionD = document.createElement('option')
-const optionE = document.createElement('option')
-const optionF = document.createElement('option')
-const labelCU = document.createElement('label')
-const inputCU = document.createElement('input')
-labelCourse.textContent = 'Course code/title:'
-inputCourse.setAttribute('placeholder', 'Course Code/Title')
+// const formGroup = document.createElement('div')
+// const labelCourse = document.createElement('label')
+// const inputCourse = document.createElement('input')
+// const labelGP = document.createElement('label')
+// const select = document.createElement('select')
+// const optionA = document.createElement('option')
+// const optionB = document.createElement('option')
+// const optionC = document.createElement('option')
+// const optionD = document.createElement('option')
+// const optionE = document.createElement('option')
+// const optionF = document.createElement('option')
+// const labelCU = document.createElement('label')
+// const inputCU = document.createElement('input')
+// labelCourse.textContent = 'Course code/title:'
+// inputCourse.setAttribute('placeholder', 'Course Code/Title')
 
 const appendDiv = () => {
-    form.append(formGroup)
-    formGroup.append(labelCourse)
-    formGroup.append(inputCourse)
-    formGroup;
+    form.innerHTML += `
+        <div class="form-group">
+            <label for="course-code">Course code/title:</label>
+            <input type="text" name="" placeholder="Course Code/Title">
+            <label for="grade-points">Grade Points:</label>
+            <select name="gp" >
+                <option value="">Choose Grade point</option>
+                <option value="5">A</option>
+                <option value="4">B</option>
+                <option value="3">C</option>
+                <option value="2">D</option>
+                <option value="1">E</option>
+                <option value="0">F</option>
+            </select>
+            <label for="credit-unit">Credit Unit</label>
+            <input type="text" name="cu" placeholder="Credit Unit">
+        </div>`;
 };
 
-// console.log(appendDiv())
 addCourseBtn.addEventListener('click', appendDiv)
